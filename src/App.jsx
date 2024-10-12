3ddefb1b3508d4627ac3c97a1dfb20611bfa88c6
Muhammed Easa <easanedumangad@gmail.com>
2024-10-12 14:17:26 +0530

    Navbar Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import Nav from "./components/Nav";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 function App() {
-
   return (
     <>
-    <h1 className="text-3xl text-red-700 font-bold underline">
-      Hello world!
-    </h1>
+      <Nav />
+      <div className="fixed h-[70vh] bottom-0 w-24 flex flex-col justify-between items-center p-10">
+        <div className="flex items-center gap-8 -rotate-90">
+          <p>Eng</p>
+          <div className="w-20 h-[1px] bg-white/50"></div>
+          <p>Ar</p>
+        </div>
+        <div className="space-y-8 [&>8]:cursor-pointer">
+          <FaLinkedin className="hover:text-orange-500" />
+          <FaGithub className="hover:text-orange-500" />
+          <FaInstagram className="hover:text-orange-500" />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
